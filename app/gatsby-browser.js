3f8a5d41485a3965a1b9c85525d90f8764361318
Jean Cochrane (Lead developer, DataMade) <jean.cochrane@datamade.us>
2020-04-24 16:37:17 -0500

    Fix style import issue

--- app/gatsby-browser.js
+++ app/gatsby-browser.js
@@ -1,1 +1,4 @@
-import "@elastic/react-search-ui-views/lib/styles/styles.css"
+// Fix for messed up package.json in search-ui-views -- see:
+// https://github.com/gatsbyjs/gatsby/issues/19446#issuecomment-552982955
+import styles from "@elastic/react-search-ui-views/lib/styles/styles.css"
+if (!styles) {console.log('Forcing include of styles')}
